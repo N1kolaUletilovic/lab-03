d664c87274372c7a46c06115a19521e72cc5dc0c
N1kolaUletilovic <uletilov@ualberta.ca>
2026-09-18 08:58:45 -0600

    add solution

--- code/app/src/main/java/com/example/listycity3/CityRepository.kt
+++ code/app/src/main/java/com/example/listycity3/CityRepository.kt
@@ -1,12 +1,34 @@
+
 package com.example.listycity3
+import androidx.compose.runtime.mutableStateListOf
+import kotlin.collections.listOf
 
 class CityRepository {
-    private val _cities = listOf(
+    private val _cities = mutableStateListOf(
         City("Edmonton", "AB"),
         City("Vancouver", "BC"),
+        City("Toronto", "ON"),
+        City("Red Deer", "AB"),
+        City("St. Albert", "AB"),
         City("Toronto", "ON")
     )
 
+
     val cities: List<City>
         get() = _cities
+
+
+    fun addCity(city: City) {
+        _cities.add(city)
+    }
+
+
+    fun updateCity(prevCity: City, newCity: City) {
+        val index = _cities.indexOf(prevCity)
+        if (index != -1) {
+            _cities[index] = newCity
+        }
+    }
 }
+
+
--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -30,7 +30,11 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CityListScreen(
                         cities = cityRepository.cities,
-                        modifier = Modifier.padding(innerPadding)
+                        onAddCity = { cityRepository.addCity(it) },
+                        modifier = Modifier.padding(innerPadding),
+                        onUpdateCity = { prevCity, newCity ->
+                            cityRepository.updateCity(prevCity, newCity)
+                        },
                     )
                 }
             }
